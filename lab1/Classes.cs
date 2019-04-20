74c4d710929a1ca5013d2d32f1e40b10caab0bc0
SG_Muwa <motherlode.muwa@gmail.com>
2019-04-20 14:02:11 +0300

    Обозначил наследования классов

---
+++ lab1/Classes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class ЗемляИПочва
+    {
+    }
+    abstract class Сотрудник
+    {
+        class Менеджер : Сотрудник
+        {
+
+        }
+        class Пахарь : Сотрудник
+        {
+
+        }
+        class Инжерен : Сотрудник
+        {
+
+        }
+    }
+    abstract class Рукотворство
+    {
+        class Здания : Рукотворство
+        {
+
+        }
+        class ИнструментыИТехника : Рукотворство
+        {
+
+        }
+        abstract class БыстрыеРесурсы : Рукотворство
+        {
+            class Семена : БыстрыеРесурсы
+            {
+
+            }
+            class Жидкости : БыстрыеРесурсы
+            {
+
+            }
+        }
+    }
+    class КнижкаЗаказов
+    {
+
+    }
+}
