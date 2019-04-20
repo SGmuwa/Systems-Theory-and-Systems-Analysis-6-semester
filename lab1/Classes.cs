35b68b26e56ddfefcb6233b461e4da0dc05f637b
SG_Muwa <motherlode.muwa@gmail.com>
2019-04-20 15:52:28 +0300

    Пробую как-нибудь красиво печатать.

--- lab1/Classes.cs
+++ lab1/Classes.cs
@@ -1,62 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace lab1
 {
+    [DataContract]
     public class ЗемляИПочва
     {
+        [DataMember]
         public DateTime ДатаПоследнегоИспользования;
+        [DataMember]
         public long ИндентификаторПоложенияЗемли;
     }
+    [DataContract]
     public abstract class Сотрудник
     {
+        [DataMember]
         public DateTime ДатаПринятияНаРаботу;
+        [DataMember]
         public string Имя;
-        public List<Сотрудник> ОтветственныйЗа = new List<Сотрудник>();
+        [DataMember]
+        public List<object> ОтветственныйЗа = new List<object>();
+        [DataMember]
         public long ТелефонСотрудника;
+        [DataContract]
         public class Менеджер : Сотрудник
         {
+            [DataMember]
             public long ТелефонПоКоторомуЗвонятКлиенты;
         }
+        [DataContract]
         public class Пахарь : Сотрудник
         {
 
         }
+        [DataContract]
         public class Инжерен : Сотрудник
         {
 
         }
     }
+    [DataContract]
     public abstract class Рукотворство
     {
+        [DataMember]
         public DateTime ДатаИзмененияСостояния;
+        [DataMember]
         public List<Сотрудник> ИсточникПоследнегоИзменения = new List<Сотрудник>();
+        [DataMember]
         public double КоличественноеИзменение = 0;
+        [DataContract]
         public class Здания : Рукотворство
         {
+            [DataMember]
             public string Адрес;
         }
+        [DataContract]
         public class ИнструментыИТехника : Рукотворство
         {
+            [DataMember]
             public string Производитель;
+            [DataMember]
             public List<БыстрыеРесурсы> Топливо;
         }
+        [DataContract]
         public abstract class БыстрыеРесурсы : Рукотворство
         {
+            [DataContract]
             public class Семена : БыстрыеРесурсы
             {
 
             }
+            [DataContract]
             public class Жидкости : БыстрыеРесурсы
             {
 
             }
         }
     }
+    [DataContract]
     public class КнижкаЗаказов
     {
+        [DataMember]
         public DateTime ДатаДобавления;
+        [DataMember]
         public List<Сотрудник.Менеджер> КтоДобавил = new List<Сотрудник.Менеджер>();
+        [DataMember]
         public long ТелефонДляСвязиСКлиентом;
     }
 }
--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,16 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace lab1
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            List<object> list = new List<object>(new object[]{
+                new ЗемляИПочва() { ДатаПоследнегоИспользования = new DateTime(1, 1, 2, 1, 0, 0), ИндентификаторПоложенияЗемли = 1 },
+                new ЗемляИПочва() { ДатаПоследнегоИспользования = new DateTime(1, 1, 2, 1, 0, 0), ИндентификаторПоложенияЗемли = 2 },
 
+                new Сотрудник.Менеджер() { ДатаПринятияНаРаботу = new DateTime(1, 1, 2, 1, 0, 0), Имя = "Рокаов Еаоу Окшалц", ТелефонПоКоторомуЗвонятКлиенты = 10485930485, ТелефонСотрудника = 2949850395903 },
+                new Сотрудник.Инжерен() { ДатаПринятияНаРаботу = new DateTime(1, 2, 2, 1, 0, 0), Имя = "Шопок Оаклу Воалк", ТелефонСотрудника = 238348732832 },
+                new Сотрудник.Пахарь() { ДатаПринятияНаРаботу = new DateTime(1, 2, 3, 1, 0, 0), Имя = "Окоеш Акорп Оекгв", ТелефонСотрудника = 12923949393 },
+
+                new Рукотворство.Здания() { Адрес = "Огкипа 5", ДатаИзмененияСостояния = new DateTime(1, 2, 2, 1, 0, 0), КоличественноеИзменение = 1.0f }
+            });
+            /*
+            list.FindClasses((Сотрудник.Пахарь t) => t.Имя == "Окоеш Акорп Оекгв")
+                .ОтветственныйЗа.Add(list.FindClasses((ЗемляИПочва t) => t.ИндентификаторПоложенияЗемли == 1));
+            list.FindClasses((Сотрудник.Пахарь t) => t.Имя == "Окоеш Акорп Оекгв")
+                .ОтветственныйЗа.Add(list.FindClasses((ЗемляИПочва t) => t.ИндентификаторПоложенияЗемли == 2));
+
+            list.FindClasses((Рукотворство.Здания t) => t.Адрес == "Огкипа 5")
+                .ИсточникПоследнегоИзменения.Add(list.FindClasses((Сотрудник.Инжерен t) => t.Имя == "Шопок Оаклу Воалк"));
+                */
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(List<object>), new DataContractJsonSerializerSettings()
+            {
+                KnownTypes = new Type[]
+                { typeof(ЗемляИПочва), typeof(Сотрудник.Менеджер), typeof(Сотрудник.Инжерен), typeof(Сотрудник.Пахарь), typeof(Рукотворство.Здания) },
+                DateTimeFormat = new DateTimeFormat("dd.MM.yyyy hh:mm:ss")
+            } );
+            MemoryStream stream1 = new MemoryStream();
+            ser.WriteObject(stream1, list);
+            stream1.Position = 0;
+            Console.WriteLine(new StreamReader(stream1).ReadToEnd());
+            Console.ReadLine();
+        }
+
+        public static SEARCH FindClasses<SEARCH, T>(this List<T> list, Predicate<SEARCH> match) where SEARCH : T
+        {
+            return ((SEARCH)list.Find((T o) => o is SEARCH && match.Invoke((SEARCH)o)));
         }
     }
 }
